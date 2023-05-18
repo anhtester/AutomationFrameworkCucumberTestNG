4d30d04c64514c59645a6c47201bcb3066631380
Anh Tester <thaian.it15@gmail.com>
2023-05-18 15:32:20 +0700

    Update version 1.2.1

--- src/test/java/anhtester/com/hooks/CucumberListener.java
+++ src/test/java/anhtester/com/hooks/CucumberListener.java
@@ -1,38 +1,20 @@
 package anhtester.com.hooks;
 
-import anhtester.com.constants.FrameworkConstants;
-import anhtester.com.helpers.CaptureHelpers;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityBuilder;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.gherkin.model.Given;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 import io.cucumber.plugin.EventListener;
 import io.cucumber.plugin.event.*;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
-
 public class CucumberListener implements EventListener {
 
+    //Khai báo các biến để thống kê tổng số test cases sau khi kết thúc test
     public static int count_totalTCs;
     public static int count_passedTCs;
     public static int count_skippedTCs;
     public static int count_failedTCs;
-
-    public CucumberListener() {
-    }
-
+    
     @Override
     public void setEventPublisher(EventPublisher publisher) {
-        /*
-         * :: is method reference , so this::collecTag means collectTags method in
-         * 'this' instance. Here we says runStarted method accepts or listens to
-         * TestRunStarted event type
-         */
+
+        //Set all events that need use (public). Override from EventListener
         publisher.registerHandlerFor(TestRunStarted.class, this::runStarted);
         publisher.registerHandlerFor(TestRunFinished.class, this::runFinished);
         publisher.registerHandlerFor(TestSourceRead.class, this::featureRead);
@@ -44,17 +26,15 @@ public class CucumberListener implements EventListener {
     }
 
     /*
-     * Here we set argument type as TestRunStarted if you set anything else then the
-     * corresponding register shows error as it doesn't have a listner method that
-     * accepts the type specified in TestRunStarted.class
+     * Khai báo các hàm đã set events bên trên để lắng nghe từng event
      */
 
-    // Here we create the reporter
+    // Here we create the reporter. Execute before run feature file
     private void runStarted(TestRunStarted event) {
 
     }
 
-    // TestRunFinished event is triggered when all feature file executions are completed
+    // runFinished event is triggered when all feature file executions are completed
     private void runFinished(TestRunFinished event) {
 
     }
@@ -68,8 +48,6 @@ public class CucumberListener implements EventListener {
         System.out.println("Feature Name: " + featureName);
     }
 
-    // This event is triggered when Test Case is started
-    // here we create the scenario node
     private void ScenarioStarted(TestCaseStarted event) {
         String featureName = event.getTestCase().getUri().toString();
         System.out.println("Feature Name: " + featureName);
@@ -80,23 +58,25 @@ public class CucumberListener implements EventListener {
     private void ScenarioFinished(TestCaseFinished event) {
         String featureName = event.getTestCase().getUri().toString();
 
-        if (event.getResult().getStatus().toString() == "PASSED") {
+        //Tại đây thì chúng ta thống kê các Scenario. Vì trong Cucumber thì một test cases là một Scenario
+        if (event.getResult().getStatus().toString().equals("PASSED")) {
             count_passedTCs = count_passedTCs + 1;
-        } else if (event.getResult().getStatus().toString() == "SKIPPED") {
-            count_skippedTCs = count_skippedTCs + 1;
-        } else {
+        }
+        if (event.getResult().getStatus().toString().equals("FAILED")) {
             count_failedTCs = count_failedTCs + 1;
+        }
+        if (event.getResult().getStatus().toString().equals("SKIPPED")) {
+            count_skippedTCs = count_skippedTCs + 1;
         }
     }
 
-    // step started event
-    // here we create the test node
+    // Step started event
     private void stepStarted(TestStepStarted event) {
 
         String stepName = "";
         String keyword = "";
 
-        // We check whether the event is from a hook or step
+        // Check whether the event is from a hook or step
         if (event.getTestStep() instanceof PickleStepTestStep) {
             // TestStepStarted event implements PickleStepTestStep interface
             // Which have additional methods to interact with the event object
@@ -109,18 +89,20 @@ public class CucumberListener implements EventListener {
             // Same with HookTestStep
             HookTestStep hoo = (HookTestStep) event.getTestStep();
             stepName = hoo.getHookType().name();
-
         }
 
     }
 
-    // This is triggered when TestStep is finished
+    // This is triggered when test Step is finished
     private void stepFinished(TestStepFinished event) {
-        if (event.getResult().getStatus().toString() == "PASSED") {
+        //Các bạn có thể screenshot hay như thế nào đó tuỳ ý cho mỗi step với trạng thái tương ứng
+        if (event.getResult().getStatus().toString().equals("PASSED")) {
 
-        } else if (event.getResult().getStatus().toString() == "SKIPPED") {
+        }
+        if (event.getResult().getStatus().toString().equals("FAILED")) {
 
-        } else {
+        }
+        if (event.getResult().getStatus().toString().equals("SKIPPED")) {
 
         }
     }
